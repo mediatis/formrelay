b4d04dc6e53eeaa4bec5952c6b923d98a76d4bd8
Michael Vöhringer <voehringer@mediatis.de>
2016-10-14 17:26:26 +0200

    Activate Logging

--- Classes/Service/FormrelayManager.php
+++ Classes/Service/FormrelayManager.php
@@ -41,14 +41,13 @@ class FormrelayManager
 		// Todo:: use ConfigutationManager to load settings
 		//
 		$typoScript = FormrelayUtility::loadPluginTS('tx_formrelay');
-		$this->settings = $typoScript['.settings'];
-		// GeneralUtility::devLog('MailFormPostProcessor:process settings', __CLASS__, 0, $typoScript);
+		$this->settings = $typoScript['settings.'];
 	}
 
 	public function process($data)
 	{
 		$this->getAdditionalData($data);
-		$this->logData($data);
+		$this->logData('form submission received', $data);
 		$this->callPlugins($data);
 		// GeneralUtility::devLog('MailFormPostProcessor:process data', __CLASS__, 0, $data);
 	}
@@ -94,10 +93,44 @@ class FormrelayManager
 		}
 	}
 
+	private function logData($message, $data=false, $error=false) {
 
-	private function logData()
-	{
-		GeneralUtility::devLog('TODO LogData', __CLASS__, 0);
+		$logfileBase = $this->settings['logfile.']['basePath'];
+
+
+		// Only write a logfile if path is set in TS Config and logdata is not empty
+		if (strlen($logfileBase) > 0) {
+
+			$logfilePath = $logfileBase . DIRECTORY_SEPARATOR . $this->settings['logfile.']['system'] . '.xml';
+
+			$xmlLog = simplexml_load_string("<?xml version=\"1.0\" encoding=\"UTF-8\"?><log />");
+			$xmlLog->addAttribute('type', $error ? 'error' : 'notice');
+			$xmlLog->addChild('logdate', date('r'));
+			$xmlLog->addChild('userIP', $_SERVER['REMOTE_ADDR']);
+
+			if ($data) {
+				$xmlFields = $xmlLog->addChild('form');
+				foreach ($data as $key => $value) {
+					if (is_array($value)) {
+						$value = implode(',', $value);
+					}
+					$xmlField = $xmlFields->addChild('field', FormrelayUtility::xmlentities($value));
+					$xmlField->addAttribute('name', FormrelayUtility::xmlentities($key));
+				}
+			}
+
+			$logdata = $xmlLog->asXML();
+
+			// open logfile and place cursor at the end of file
+			if($logfile = @fopen($logfilePath, "a")) {
+				// write xml to logfile and close it
+				@fwrite($logfile, $logdata);
+				fclose($logfile);
+			} else {
+				debug("Could not open file at ". $logfilePath);
+			}
+		}
 	}
+
 }
 ?>
