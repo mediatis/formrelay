eab5ae2f0e752b3d3ed93841d7773b3eed1b1a72
Stephan Ude <ude@mediatis.de>
2018-04-10 15:59:14 +0200

    TYPO3 8 ready (dependencies updated)

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.1.7',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
         ),
         'conflicts' => array(
         ),
