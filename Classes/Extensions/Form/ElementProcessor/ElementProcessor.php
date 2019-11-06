b5ac754178f6048ad3b65f7ee1aac2fd9468258c
Stephan Ude <ude@mediatis.de>
2019-11-06 19:37:00 +0100

    [BUGFIX] fixed formatting of form field datepicker for ext:form

--- Classes/Extensions/Form/ElementProcessor/ElementProcessor.php
+++ Classes/Extensions/Form/ElementProcessor/ElementProcessor.php
@@ -17,19 +17,52 @@ abstract class ElementProcessor implements ElementProcessorInterface
         $this->configurationManager = $configurationManager;
     }
 
-    abstract protected function getElementClass();
-
     abstract protected function process($element, $elementValue);
+
+    protected function getElementClass()
+    {
+        return '';
+    }
+
+    protected function getElementType()
+    {
+        return '';
+    }
+
+    protected function getValueClass()
+    {
+        return '';
+    }
 
     protected function match($element, $elementValue)
     {
-        return is_a($element, $this->getElementClass());
+        $elementClass = $this->getElementClass();
+        if ($elementClass && is_a($element, $elementClass)) {
+            return true;
+        }
+
+        $elementType = $this->getElementType();
+        if ($elementType && $element->getType() === $elementType) {
+            return true;
+        }
+
+        $valueClass = $this->getValueClass();
+        if ($valueClass && is_a($elementValue, $valueClass)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function override()
+    {
+        return false;
     }
 
     public function processFormElement($element, $elementValue, array $options, array &$result, bool &$processed)
     {
         $this->options = $options;
-        if (!$processed && $this->match($element, $elementValue)) {
+        if ((!$processed || $this->override()) && $this->match($element, $elementValue)) {
             $id = $element->getIdentifier();
             $name = $element->getProperties()['fluidAdditionalAttributes']['name'] ?: $id;
             $value = $this->process($element, $elementValue);
