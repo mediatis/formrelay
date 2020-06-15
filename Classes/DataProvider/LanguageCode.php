3de15f51c1e33b11a30f2cf19eaa6775b253879a
Stephan Ude <ude@mediatis.de>
2020-06-15 19:04:10 +0200

    [BUGFIX] restored compatibility for typo3 9

--- Classes/DataProvider/LanguageCode.php
+++ Classes/DataProvider/LanguageCode.php
@@ -2,10 +2,18 @@
 
 namespace Mediatis\Formrelay\DataProvider;
 
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
 class LanguageCode implements DataProviderInterface
 {
     public function addData(array &$dataArray)
     {
-        $dataArray['language'] = $GLOBALS['TSFE']->getLanguage()->getTwoLetterIsoCode();
+        if (VersionNumberUtility::convertVersionNumberToInteger(
+            VersionNumberUtility::getNumericTypo3Version()
+        ) >= 10000000) {
+            $dataArray['language'] = $GLOBALS['TSFE']->getLanguage()->getTwoLetterIsoCode();
+        } else {
+            $dataArray['language'] = $GLOBALS['TSFE']->tmpl->setup['config.']['language'];
+        }
     }
 }
